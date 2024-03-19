53d1ec598696102b384c4ab82b8502423342eaf1
masonspacestation <71409121+masonspacestation@users.noreply.github.com>
2024-03-19 12:35:36 -0600

    initial

---
+++ app.js
@@ -0,0 +1,34 @@
+console.log("🍨")
+
+let iceCream = [{
+  name: 'Cookie Dough',
+  price: 1.25,
+  quantity: 0
+},
+{
+  name: 'Vanilla',
+  price: 1,
+  quantity: 0
+},
+{
+  name: 'Strawberry',
+  price: 1.25,
+  quantity: 0
+}]
+
+let toppings = [{
+  name: 'Sprinkles',
+  quantity: 0,
+  price: .25
+},
+{
+  name: 'Chocolate Chips',
+  price: .25,
+  quantity: 0
+},
+{
+  name: 'Cookie Chunks',
+  price: .5,
+  quantity: 0
+}]
+
